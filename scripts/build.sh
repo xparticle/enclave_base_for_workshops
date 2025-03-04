5ed7b43ab6a56d457fe35a38a536f60737d77f98
Sudhir Reddy <sudhirreddym@gmail.com>
2025-03-04 16:18:48 -0500

    Add build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+set -e
+
+echo "Build started on $(date)"
+echo "Building the enclave base Docker image..."
+docker build --cache-from "${BASE_REPOSITORY_URI}:latest" -f Dockerfile.base -t "${BASE_REPOSITORY_URI}:latest" -t enclave_base .
