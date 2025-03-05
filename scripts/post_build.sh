37dc40ceabb3bfcc195de7aca24761bd0cdfcb37
Sudhir Reddy <sudhirreddym@gmail.com>
2025-03-05 10:08:24 -0500

    Update post_build.sh

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -2,7 +2,8 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 set -e
-
+# Set repository URI
+BASE_REPOSITORY_URI="${ECR_BASE_REPO_URI}"
 echo "Build completed on $(date)"
 echo "Pushing the Docker images..."
 docker push "${BASE_REPOSITORY_URI}:latest"
