24710be711a1419ddc3e38cfae2fec5a06863aa7
Sudhir Reddy <sudhirreddym@gmail.com>
2025-03-05 10:08:02 -0500

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: MIT-0
 set -e
 
+export BASE_REPOSITORY_URI="${ECR_BASE_REPO_URI}"
 echo "Build started on $(date)"
 echo "Building the enclave base Docker image..."
 docker build --cache-from "${BASE_REPOSITORY_URI}:latest" -f Dockerfile.base -t "${BASE_REPOSITORY_URI}:latest" -t enclave_base .
